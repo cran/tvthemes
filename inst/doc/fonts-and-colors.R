2fe5713cf00187696a273c8e001b259968719f35
Ryo Nakagawara <ryonakagawara@gmail.com>
2022-07-19 05:10:02 +0000

    version 1.3.1

--- inst/doc/fonts-and-colors.R
+++ inst/doc/fonts-and-colors.R
@@ -12,11 +12,7 @@ knitr::opts_chunk$set(
 #  import_simpsons()         ## "Akbar" font
 #  import_avatar()           ## "Slayer" font (formerly `theme_theLastAirbender()`)
 #  import_rickAndMorty()     ## "Get Schwifty" font
-#  import_robotoCondensed()  ## "Roboto Condensed" Google Font import from hrbrthemes
-#  import_titilliumWeb()     ## "Titillium Web" Google Font import from hrbrthemes
 #  import_spongeBob()        ## "Some Time Later" font
-#  import_cinzel()           ## "Cinzel" font to use with 'The Palettes of Ice & Fire'
-#  import_chelseaMarket()    ## "Chelsea Market" font for "Hilda"
 #  import_gravitationFalls() ## "Gravitation Falls" font for "Gravity Falls"
 #  
 #  ## install.packages("extrafont")
--- inst/fonts/GravitationFalls/readme.txt
+++
@@ -1,8 +0,0 @@
-Gravitation Falls FONT
-
-Gravitation Falls is a font replicating the font used for the logo of the TV show "Gravity Falls".
-
-Gravity Falls is owned by Alex Hirsch and Disney.
-
-(c) 2015 - MaxiGamer - maxigamer.deviantart.com
-Made with FontForge.
